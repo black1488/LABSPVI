6674654690df87feb432e2ca2ca3aa999e6b4ccc
Ihor Chornenkyi <darkwhite227@gmail.com>
2022-05-02 16:32:41 +0300

    last preparations of lab3

--- lab1/diagram.js
+++ lab1/diagram.js
@@ -1,42 +1,105 @@
+var figuresArray = [0,0,0,0,0,0,0,0,0,0,0,0];
+var myChart;
+
+function moveFigure(){
+  var cells = document.querySelectorAll('img[class=figure]');
+  for (let index = 0; index < cells.length; index++) {
+    cells[index].addEventListener("click",function(){
+        this.remove();
+    });
+  }
+}
+
+function zeroFigures(){
+    figuresArray = [0,0,0,0,0,0,0,0,0,0,0,0];
+}
+function checkFigures(){
+  zeroFigures();
+  var figures = document.querySelectorAll('img[class=figure]');
+  var dynpath = window.location.href.replace('index.html','figures');
+  for (let index = 0; index < figures.length; index++) {
+      if (figures[index].src==dynpath+"/wb.png") {
+          figuresArray[0]++;
+      }
+      if (figures[index].src==dynpath+"/wk.png") {
+          figuresArray[1]++;
+      }
+      if (figures[index].src==dynpath+"/wn.png") {
+          figuresArray[2]++;
+      }
+      if (figures[index].src==dynpath+"/wp.png") {
+          figuresArray[3]++;
+      }
+      if (figures[index].src==dynpath+"/wq.png") {
+          figuresArray[4]++;
+      }
+      if (figures[index].src==dynpath+"/wr.png") {
+          figuresArray[5]++;
+      }
+      if (figures[index].src==dynpath+"/bb.png") {
+          figuresArray[6]++;
+      }
+      if (figures[index].src==dynpath+"/bk.png") {
+          figuresArray[7]++;
+      }
+      if (figures[index].src==dynpath+"/bn.png") {
+          figuresArray[8]++;
+      }
+      if (figures[index].src==dynpath+"/bp.png") {
+          figuresArray[9]++;
+      }
+      if (figures[index].src==dynpath+"/bq.png") {
+          figuresArray[10]++;
+      }
+      if (figures[index].src==dynpath+"/br.png") {
+          figuresArray[11]++;
+      }
+  }
+}
+
 function createDiagram() {
-    var wb =0;
-    var wk =0;
-    var wn =0;
-    var wp =0;
-    var wq =0;
-    var wr =0;
-    var bb =0;
-    var bk =0;
-    var bn =0;
-    var bp =0;
-    var bq =0;
-    var br =0;
-    //навчитися зчитувати дані
-    var table = document.getElementById("table");
-    for (var i = 0, row; row=table.rows[i]; i++) {
-        for (var j = 0, col;  col = row.cells[j]; j++) {
-            var item;
-        }
-    }
-    // Bar chart
-  new Chart(document.getElementById("chess-chart"), {
+  checkFigures();
+  myChart = new Chart(document.getElementById("chess-chart"), {
   type: 'bar',
   data: {
-    labels: ["wb", "wk", "wn", "wp", "wq", "wr", "bb", "bk", "bn", "bp", "bq", "br"],
+    labels: ["white bishop", "white king", "white knight", "white pawn", "white queen", "white rock", "black bishop", "black king", "black knight", "black pawn", "black queen", "black rock"],
     datasets: [
       {
         label: "Pieces",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850","#c45850","#3e95cd"],
-        data: [2,1,2,8,1,2,2,1,2,8,1,2]
+        backgroundColor: ["#ffffff", "#ffffff","#ffffff","#ffffff","#ffffff","#ffffff", "#000000","#000000","#000000","#000000","#000000","#000000"],
+        data: [figuresArray[0], figuresArray[1], figuresArray[2], figuresArray[3], figuresArray[4], figuresArray[5], figuresArray[6], figuresArray[7], figuresArray[8], figuresArray[9], figuresArray[10], figuresArray[11]]
       }
     ]
   }, 
   options: {
-    legend: { display: false },
+    scales:{
+        yAxes:[{
+            ticks:{
+              beginAtZero: true,
+              fontSize: 20
+            }
+        }],
+        xAxes:[{
+            ticks:{
+              fontSize:20
+            }
+        }]
+    },
+    legend: {
+        display: false 
+    },
     title: {
       display: true,
-      text: 'My chess diagram'
+      text: 'My chess diagram',
+      fontSize: 20
     }
   }
   });
+}
+function refreshDiagram(){
+    checkFigures();
+    for (let index = 0; index < figuresArray.length; index++) {
+        myChart.data.datasets[0].data[index] = figuresArray[index];
+    }
+    myChart.update();
 }
